161ddc2d35360162891589c71d3d60fe95e7ce8b
djfin <djfinley98@gmail.com>
2019-11-07 21:03:48 -0700

    pokemon class

---
+++ src/app/pokemon.ts
@@ -0,0 +1,11 @@
+export class Pokemon{
+    id: number;
+    name: string;
+    form: string;
+    summary: string;
+    height: number;
+    category: string;
+    weight: number;
+    abilities: string;
+    gender: string;
+}
