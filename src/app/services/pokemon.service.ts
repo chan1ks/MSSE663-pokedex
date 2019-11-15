b0d7dee19dcf48e3f857a30cda8387545f22948e
Morgan Worrell <c-morgan.worrell@charter.com>
2019-11-15 11:15:59 -0700

    initial service

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  constructor(private http: HttpClient) {}
+
+  getAllPokemon(): Observable<any> {
+    return this.http
+      .get('https://pokeapi.co/api/v2/pokemon/')
+      .pipe(map((data: any) => data.results));
+  }
+}
